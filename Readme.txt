a801db8b3469ccea1107b14425ede76cab5bec37
10700H <99000966+10700H@users.noreply.github.com>
2025-01-20 13:47:04 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Author: 10700H
+Extension: Q-Gen
+Description: Q-Gen is a Qr code generator for Firefox and this is similar to chrome and edge's ability to generate QR Codes.
+Features: Generates QR code for any Browser Tab, additionally you can download and copy the QR code as per your need.
+
+Thanks To 
+Icons8
---
+++ popup.js
@@ -0,0 +1,52 @@
+import "./qrcode/qrcode.js";
+
+var qr = new QRCode(document.getElementById("qrarea"), {
+  width: 250,
+  height: 250,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    let currentTab = tabs[0];
+    checkUrl(currentTab.url);
+  }).catch((error) => {
+    console.error("Error getting current tab URL:", error);
+  });
+});
+
+
+document.getElementById('download').addEventListener('click', () => {
+  let canvas = document.querySelector('#qrarea canvas');
+  let img = canvas.toDataURL("image/png");
+  let link = document.createElement('a');
+  link.href = img;
+  link.download = 'qrcode.png';
+  link.click();
+});
+
+document.getElementById('copy').addEventListener('click', () => {
+  let canvas = document.querySelector('#qrarea canvas');
+  canvas.toBlob(blob => {
+    const item = new ClipboardItem({ 'image/png': blob });
+    navigator.clipboard.write([item]);
+    browser.window.alert("Copied to clipboard");
+  });
+});
+
+function checkUrl(url) {
+  fetch('URLs/ExcludedUrl.json').then(response => response.json()).then(data => {
+      if (data.excludedUrls.includes(url)) {
+      document.getElementById('qrtext').innerHTML = 'QR cannot be generated for this link';
+      document.getElementById('download').disabled = true;
+      document.getElementById('copy').disabled = true;
+      } else {
+      qr.makeCode(url);
+      document.getElementById('download').disabled = false;
+      document.getElementById('copy').disabled = false;
+      }
+    })
+    .catch(error => console.error('Error fetching the JSON file:', error));
+}
